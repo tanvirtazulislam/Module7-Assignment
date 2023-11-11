6ef7a8e6d97a6c429f77b35a617136eda3bdc736
tanvirtazulislam <tanvirtazul@gmail.com>
2023-11-11 20:26:42 +0600

    Module 7 Assignment

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    function helloLaravel(){
+        return "Hello, Laravel!";
+    }
+}
